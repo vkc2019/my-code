7b7c4775f2c04842273baa37eb9da09d9a5b248c
Kishore Chilakala <kishor.chilakala@logituit.com>
2024-01-24 21:28:20 +0530

    Initial commit

---
+++ languages/nodejs/01-node-data-types.js
@@ -0,0 +1,17 @@
+// Node.js has a few core types: number , boolean , string , and object.
+
+// undefined
+var a ;
+console.log(a);
+
+var b = true;
+console.log(typeof b);
+
+var c = "string";
+console.log(typeof c);
+
+var d = {};
+console.log(typeof d);
+
+var e = null ;
+console.log(e);
